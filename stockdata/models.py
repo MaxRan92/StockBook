bef0e7caac49525be45a3e8650d75f58c0228c5e
Massimo Ranalli <ranallimax@gmail.com>
2022-05-27 00:28:58 +0000

    Create Stockinfo and Comment models

--- stockdata/models.py
+++ stockdata/models.py
@@ -1,3 +1,40 @@
+import os
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class StockInfo(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    ticker = models.CharField(max_length=6, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="stock_info")
+    updated_on = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    logo_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    stock = models.ForeignKey(StockInfo, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
