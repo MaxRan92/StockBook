6d1a118e786c529bb29f418247ce2f4e705c0a71
Massimo Ranalli <ranallimax@gmail.com>
2022-05-27 01:51:21 +0000

    Add stock detail html page, view and urls

--- stockdata/urls.py
+++ stockdata/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.StockList.as_view(), name='home'),
+    path('<slug:slug>/', views.StockDetail.as_view(), name='stock_detail'),
 ]
--- stockdata/views.py
+++ stockdata/views.py
@@ -8,3 +8,20 @@ class StockList(generic.ListView):
     queryset = StockInfo.objects.filter(status=1).order_by("-created_on")
     template_name = 'index.html'
     paginate_by = 6
+
+
+class StockDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = StockInfo.objects.filter(status=1)
+        stockinfo = get_object_or_404(queryset, slug=slug)
+        comments = stockinfo.comments.filter(approved=True).order_by('created_on')
+        
+        return render(
+            request,
+            "stock_detail.html",
+            {
+                "stockinfo": stockinfo,
+                "comments": comments,
+            },
+        )
