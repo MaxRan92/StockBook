add8b1548b50b4d03e77a997efb333c7833ead49
Massimo Ranalli <ranallimax@gmail.com>
2022-05-28 02:35:08 +0000

    Add insert comment function

--- stockdata/views.py
+++ stockdata/views.py
@@ -16,7 +16,7 @@ class StockDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = StockInfo.objects.filter(status=1)
         stockinfo = get_object_or_404(queryset, slug=slug)
-        comments = stockinfo.comments.filter(approved=True).order_by('created_on')
+        comments = stockinfo.comments.filter(approved=True).order_by('-created_on')
         
         return render(
             request,
@@ -24,6 +24,34 @@ class StockDetail(View):
             {
                 "stockinfo": stockinfo,
                 "comments": comments,
-                "comment_form": CommentForm()
+                "commented": False,
+                "comment_form": CommentForm,
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = StockInfo.objects.filter(status=1)
+        stockinfo = get_object_or_404(queryset, slug=slug)
+        comments = stockinfo.comments.filter(approved=True).order_by('-created_on')
+        
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.stockinfo = stockinfo
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "stock_detail.html",
+            {
+                "stockinfo": stockinfo,
+                "comments": comments,
+                "commented": True,
+                "comment_form": CommentForm,
             },
         )
