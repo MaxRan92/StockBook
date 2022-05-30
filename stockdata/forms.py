8db7728995bbb6aa540413f7fa569705e9572c9d
Massimo Ranalli <ranallimax@gmail.com>
2022-05-30 22:26:38 +0000

    Add sentiment edit to EditForm

--- stockdata/forms.py
+++ stockdata/forms.py
@@ -10,7 +10,8 @@ class CommentForm(forms.ModelForm):
 class EditForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ("body",)
+        fields = ('body', 'sentiment')
         labels = {
-            "body": "Post a comment:"
+            "body": "Post a comment:",
+            "sentiment": "Change your sentiment:"
         }
